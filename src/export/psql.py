9fbef1f57b116e31ec02bb3d90d45328e7a93b4e
forrest <fspmarshall@gmail.com>
2017-03-31 14:33:16 -1000

    added feature to psql uploader to save ignored rows to csv

--- src/export/psql.py
+++ src/export/psql.py
@@ -15,12 +15,17 @@ def export(data,project,config):
     # default is just standard psycopg2 formatting...
     else: ins = ','.join(['%s'] * len(fields))
     cmd = 'INSERT INTO {} VALUES ({})'.format(tbl,ins)
-    errs,errtxt,duplicates = handle_push(data,cmd,db)
+    errors,errtxt,duplicates = handle_push(data,cmd,db)
     if duplicates:
-        print('duplicate rows ignored: ',duplicates)
+        print('duplicate rows ignored: ',len(duplicates))
     # save any rows which raised unexpexted errors
     # to a csv with prefix `psqlerr`.
-    if errs: errdata(project,data,txt='psqlerr')
+    if errors: errdata(project,errors,txt='psqlerr')
+    # if `save-duplicates` is flagged `true`, save
+    # all duplicate rows to the errors directory.
+    if duplicates and 'save-duplicates' in config['general']:
+        if config['general']['save-duplicates']:
+            errdata(project,duplicates,txt='psqldups')
     # append any unexpected errors to
     # the project's main error log.
     for err in errtxt: mklog(project,err)
@@ -31,24 +36,27 @@ def export(data,project,config):
 # until recently, will invalidate entire transaction
 # blocks if too many duplacates are encountered.
 def handle_push(data,cmd,db):
-    duplicates = 0
-    errs,errtxt = [],[]
-    # gotta copy `data` to avoid side-effects.
+    rowtotal = len(data)
+    # make a copy of data to avoid
+    # side-effects from pops/dels.
     rows = [r for r in data]
+    uploaded,ignored = [],[]
+    errors,errtxt = [],[]
     while len(rows) > 0:
-        rows,dup,err,txt = push_rows(rows,cmd,db)
-        if dup: duplicates += 1
-        if err: errs.append(err)
+        rows,upld,igns,err,txt = push_rows(rows,cmd,db)
+        uploaded += upld
+        ignored += igns
+        if err: errors.append(err)
         if txt: errtxt.append(txt)
-    return errs,errtxt,duplicates
+    assert rowtotal == sum(len(errors),len(ignored),len(uploaded))
+    return errors,errtxt,ignored
 
 # Attempts to push rows to db.  Removes rows
 # from data as they are pushed.  Halts & returns
 # remaining rows upon first exception.
 def push_rows(data,cmd,db):
-    duplicate = False
-    error = None
-    text = None
+    ignored,uploaded = [],[]
+    error,text = None,None
     with psql.connect(database=db) as con:
         con.set_session(autocommit=True)
         for i in range(len(data)):
@@ -56,15 +64,16 @@ def push_rows(data,cmd,db):
             try:
                 with con.cursor() as cur:
                     cur.execute(cmd,row)
+                uploaded.append(row)
             except Exception as err:
                 if 'duplicate key' in str(err):
-                    duplicate = True
+                    ignored.append(row)
                 else:
                     error = row
                     text = str(err)
                 break
     con.close()
-    return data,duplicate,error,text
+    return data,uploaded,ignored,error,text
 
 
 # Generate a custom insertion string.
